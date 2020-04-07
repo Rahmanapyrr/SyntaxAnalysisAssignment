02ab01615f8f769281f5d4deda0650ac9dc08718
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2020-04-07 18:44:12 -0400

    Update syntax_analysis.c

--- syntax_analysis.c
+++ syntax_analysis.c
@@ -48,3 +48,38 @@ main() {
  } while (nextToken != EOF);
  }
 }
+
+/*****************************************************/
+/* lookup - a function to lookup operators and parentheses
+ and return the token */
+int lookup(char ch) {
+ switch (ch) {
+  case '(':
+   addChar();
+   nextToken = LEFT_PAREN;
+   break;
+   
+ case ')':
+   addChar();
+   nextToken = RIGHT_PAREN;
+   break;
+   
+ case '+':
+  addChar();
+  nextToken = ADD_OP;
+  break;
+   
+ case '-':
+  addChar();
+  nextToken = SUB_OP;
+  break;
+ 
+ case '*':
+  addChar();
+  nextToken = MULT_OP;
+  break;
+   
+ case '/':
+  addChar();
+  nextToken = DIV_OP;
+  break;
